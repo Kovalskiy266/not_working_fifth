cdd63c0ee9031a3e0c338ddd21568f2541244221
Kovalskiy <kovalevskiiy16@ukr.net>
2021-07-12 00:57:52 +0300

    made a content part

---
+++ js/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        arrows: false,
+        dots: true, 
+        infinite: true,
+        draggable: true,
+        pauseOnHover: true,
+        swipeToSlide: true,
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        autoplay: false,
+        autoplaySpeed: 2000,
+        // initialSlide: 1
+        // variableWidth: true
+        // 
+
+    });
+});
