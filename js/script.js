01391890faf0e3cbdb00be22d7c93ffd4a7f26f7
Kovalskiy <kovalevskiiy16@ukr.net>
2021-07-13 20:38:40 +0300

    Made a pixel perfect

--- js/script.js
+++ js/script.js
@@ -3,13 +3,12 @@ $(document).ready(function(){
         arrows: false,
         dots: true, 
         infinite: true,
-        draggable: true,
-        pauseOnHover: true,
-        swipeToSlide: true,
         slidesToShow: 2,
-        slidesToScroll: 2,
+        slidesToScroll: 1,
         autoplay: false,
         autoplaySpeed: 2000,
+        // centerMode: true,
+        // centerPadding: '10px'
         // initialSlide: 1
         // variableWidth: true
         // 
