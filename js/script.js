dc87994ba2760b2801b5371cdd4b6dce7ab0a52a
Kovalskiy <kovalevskiiy16@ukr.net>
2021-07-16 01:01:05 +0300

    made a form

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
 $(document).ready(function(){
+       
+    $('.menu-burger__header').click(function() {
+        $('.menu-burger__header').toggleClass('open-menu');
+        $('.header__nav').toggleClass('open-menu');
+        $('body').toggleClass('fixed-page');
+    });
+    
     $('.slider').slick({
         arrows: false,
         dots: true, 
@@ -7,11 +14,52 @@ $(document).ready(function(){
         slidesToScroll: 1,
         autoplay: false,
         autoplaySpeed: 2000,
-        // centerMode: true,
-        // centerPadding: '10px'
-        // initialSlide: 1
-        // variableWidth: true
-        // 
-
+        responsive: [
+            {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 1
+            }
+        }
+        ]
     });
+
+    // $('.pricing_slider').slick({
+    //     arrows:false,
+    //     infinite: false, 
+    //     slidesToShow: 4,
+        // responsive:[
+        //     {
+        //         breakpoint: 1200, 
+                
+        //         settings: {
+        //             slidesToShow: 3, 
+        //             slidesToScroll: 1,
+        //             infinite: true,
+        //             dots: true,
+        //             // CenterMode: true,
+        //             // centerPadding: '10px',
+        //         }
+        //     }, 
+        //     {
+        //         breakpoint: 768,
+        //         settings: {
+        //             slidesToShow: 2, 
+        //             slidesToScroll: 1,
+        //             infinite: true
+        //         }
+        //     },
+
+        //     {
+        //         breakpoint: 768,
+        //         settings: {
+        //             slidesToShow: 1, 
+        //             slidesToScroll: 1,
+        //             infinite: true
+        //         }
+        //     }
+        // ]
+
+    // });
 });
+
