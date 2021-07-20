2e220a8562df82c82077f4b895f43dc72a1ebb5b
Kovalskiy <kovalevskiiy16@ukr.net>
2021-07-20 23:58:06 +0300

    done

--- js/script.js
+++ js/script.js
@@ -1,11 +1,9 @@
-$(document).ready(function(){
-       
+$(document).ready(function(){   
     $('.menu-burger__header').click(function() {
         $('.menu-burger__header').toggleClass('open-menu');
         $('.header__nav').toggleClass('open-menu');
         $('body').toggleClass('fixed-page');
     });
-    
     $('.slider').slick({
         arrows: false,
         dots: true, 
@@ -16,7 +14,7 @@ $(document).ready(function(){
         autoplaySpeed: 2000,
         responsive: [
             {
-            breakpoint: 992,
+            breakpoint: 1200,
             settings: {
                 slidesToShow: 1
             }
@@ -24,42 +22,5 @@ $(document).ready(function(){
         ]
     });
 
-    // $('.pricing_slider').slick({
-    //     arrows:false,
-    //     infinite: false, 
-    //     slidesToShow: 4,
-        // responsive:[
-        //     {
-        //         breakpoint: 1200, 
-                
-        //         settings: {
-        //             slidesToShow: 3, 
-        //             slidesToScroll: 1,
-        //             infinite: true,
-        //             dots: true,
-        //             // CenterMode: true,
-        //             // centerPadding: '10px',
-        //         }
-        //     }, 
-        //     {
-        //         breakpoint: 768,
-        //         settings: {
-        //             slidesToShow: 2, 
-        //             slidesToScroll: 1,
-        //             infinite: true
-        //         }
-        //     },
-
-        //     {
-        //         breakpoint: 768,
-        //         settings: {
-        //             slidesToShow: 1, 
-        //             slidesToScroll: 1,
-        //             infinite: true
-        //         }
-        //     }
-        // ]
-
-    // });
 });
 
